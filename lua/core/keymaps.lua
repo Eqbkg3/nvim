52d16be0ce52bbde5d743e9c963a161bb452e8dc
eqbkg3 <Eqbkg3@mail.com>
2023-02-12 21:41:02 +0200

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+local function map_key(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+map_key('i', 'jk', '<ESC>')
